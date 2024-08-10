4fd2a953799c8803098407cf6d8d4e491b644442
電波 <piacern@gmail.com>
2024-08-10 22:34:41 +0900

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -1,8 +1,8 @@
-//
-//  matrix.h
-//  Contains almost all the functions related to matrices.
+//  matrix.hpp
+//  Contains all the functions related to matrices.
 //  Created by 電波
-//
+
+#pragma once
 
 // -----------------------------------------------
 // @denpa: All the matrix structures below are assumed to be row-major.
@@ -10,17 +10,21 @@
 
 #define DEFINE_MATRIX_STRUCTURE(N) \
 typedef struct matrix##N##x##N { \
-	float v[N*N]; \
+	f32 v[N*N]; \
 } matrix##N##x##N;
 
 DEFINE_MATRIX_STRUCTURE(2);
 DEFINE_MATRIX_STRUCTURE(3);
 DEFINE_MATRIX_STRUCTURE(4);
 
+STATIC_ASSERT(sizeof(matrix2x2) == 2 * 2 * sizeof(f32), "Unexpected padding for matrix2x2.");
+STATIC_ASSERT(sizeof(matrix3x3) == 3 * 3 * sizeof(f32), "Unexpected padding for matrix3x3.");
+STATIC_ASSERT(sizeof(matrix4x4) == 4 * 4 * sizeof(f32), "Unexpected padding for matrix4x4.");
+
 // -----------------------------------------------
 // @denpa: Finds the submatrix of a 4 by 4 matrix.
 // -----------------------------------------------
-DINLINE matrix3x3 submatrixOf4x4(matrix4x4 a, uint32_t row, uint32_t column) {
+INTERNAL DINLINE matrix3x3 submatrixOf4x4(matrix4x4 a, u32 row, u32 column) {
 	if (row == 0) {
 		if (column == 0) {
 			return matrix3x3 {a.v[5], a.v[6], a.v[7], a.v[9], a.v[10], a.v[11], a.v[13], a.v[14], a.v[15]};
@@ -68,7 +72,7 @@ DINLINE matrix3x3 submatrixOf4x4(matrix4x4 a, uint32_t row, uint32_t column) {
 // -----------------------------------------------
 // @denpa: Finds the submatrix of a 3 by 3 matrix.
 // -----------------------------------------------
-DINLINE matrix2x2 submatrixOf3x3(matrix3x3 a, uint32_t row, uint32_t column) {
+INTERNAL DINLINE matrix2x2 submatrixOf3x3(matrix3x3 a, u32 row, u32 column) {
 	if (row == 0) {
 		if (column == 0) {
 			return matrix2x2 {a.v[4], a.v[5], a.v[7], a.v[8]};
@@ -100,54 +104,51 @@ DINLINE matrix2x2 submatrixOf3x3(matrix3x3 a, uint32_t row, uint32_t column) {
 // -----------------------------------------------
 // @denpa: Finds the determinant of a 2 by 2 matrix.
 // -----------------------------------------------
-DINLINE float determinant2x2(matrix2x2 a) {
+INTERNAL DINLINE f32 determinant2x2(matrix2x2 a) {
 	return (a.v[0]*a.v[3]) - (a.v[1]*a.v[2]);
 }
 
 // -----------------------------------------------
 // @denpa: Finds the minor of a 3 by 3 matrix.
 // -----------------------------------------------
-DINLINE float minorOf3x3(matrix3x3 a, uint32_t row, uint32_t column) {
+INTERNAL DINLINE f32 minorOf3x3(matrix3x3 a, u32 row, u32 column) {
 	return determinant2x2(submatrixOf3x3(a, row, column));
 }
 
 // -----------------------------------------------
 // @denpa: Finds the cofactor of a 3 by 3 matrix.
 // -----------------------------------------------
-DINLINE float cofactorOf3x3(matrix3x3 a, uint32_t row, uint32_t column) {
-	if ((row + column) % 2 == 0) {
-		return minorOf3x3(a, row, column);
-	} else {
-		return -(minorOf3x3(a, row, column));
-	}
+INTERNAL DINLINE f32 cofactorOf3x3(matrix3x3 a, u32 row, u32 column) {
+	if ((row + column) % 2 == 0) {return minorOf3x3(a, row, column);}
+	else {return -(minorOf3x3(a, row, column));}
 }
 
 // -----------------------------------------------
 // @denpa: Finds the determinant of a 3 by 3 matrix.
 // -----------------------------------------------
-DINLINE float determinant3x3(matrix3x3 a) {
+INTERNAL DINLINE f32 determinant3x3(matrix3x3 a) {
 	return (a.v[0] * cofactorOf3x3(a, 0, 0)) + (a.v[1] * cofactorOf3x3(a, 0, 1)) + (a.v[2] * cofactorOf3x3(a, 0, 2));
 }
 
 // -----------------------------------------------
 // @denpa: Finds the minor of a 4 by 4 matrix.
 // -----------------------------------------------
-DINLINE float minorOf4x4(matrix4x4 a, uint32_t row, uint32_t column) {
+INTERNAL DINLINE f32 minorOf4x4(matrix4x4 a, u32 row, u32 column) {
 	return determinant3x3(submatrixOf4x4(a, row, column));
 }
 
 // -----------------------------------------------
 // @denpa: Finds the cofactor of a 4 by 4 matrix.
 // -----------------------------------------------
-DINLINE float cofactorOf4x4(matrix4x4 a, uint32_t row, uint32_t column) {
+INTERNAL DINLINE f32 cofactorOf4x4(matrix4x4 a, u32 row, u32 column) {
 	if ((row + column) % 2 == 0) {return minorOf4x4(a, row, column);}
-	else {return (minorOf4x4(a, row, column));}
+	else {return -(minorOf4x4(a, row, column));}
 }
 
 // -----------------------------------------------
 // @denpa: Finds the determinant of a 4 by 4 matrix.
 // -----------------------------------------------
-DINLINE float determinant4x4(matrix4x4 a) {
+INTERNAL DINLINE f32 determinant4x4(matrix4x4 a) {
 	return (a.v[0] * cofactorOf4x4(a, 0, 0)) + (a.v[1] * cofactorOf4x4(a, 0, 1)) + (a.v[2] * cofactorOf4x4(a, 0, 2) + (a.v[3]) * cofactorOf4x4(a, 0, 3));
 }
 
@@ -155,8 +156,8 @@ DINLINE float determinant4x4(matrix4x4 a) {
 // -----------------------------------------------
 // @denpa: Checks if two 4 by 4 matrices are equal to each other.
 // -----------------------------------------------
-DINLINE bool are4x4MatricesEqual(matrix4x4 a, matrix4x4 b) {
-	for (uint_fast8_t i = 0; i < 16; i++) {
+INTERNAL DINLINE bool are4x4MatricesEqual(matrix4x4 a, matrix4x4 b) {
+	for (u8 i = 0; i < 16; i++) {
 		if (!areFloatsEqual(a.v[i], b.v[i])) {return false;}
 	}
 	return true;
@@ -165,7 +166,7 @@ DINLINE bool are4x4MatricesEqual(matrix4x4 a, matrix4x4 b) {
 // -----------------------------------------------
 // @denpa: Translates an objected by the specified coordinates.
 // -----------------------------------------------
-DINLINE matrix4x4 createTranslationMatrix(float x, float y, float z) {
+INTERNAL DINLINE matrix4x4 createTranslationMatrix(f32 x, f32 y, f32 z) {
 	return matrix4x4 {1.f, 0.f, 0.f, x,
 					0.f, 1.f, 0.f, y,
 					0.f, 0.f, 1.f, z,
@@ -175,7 +176,7 @@ DINLINE matrix4x4 createTranslationMatrix(float x, float y, float z) {
 // -----------------------------------------------
 // @denpa: Scales an object by the provided scale factors.
 // -----------------------------------------------
-DINLINE matrix4x4 createScaleMatrix(float x, float y, float z) {
+INTERNAL DINLINE matrix4x4 createScaleMatrix(f32 x, f32 y, f32 z) {
 	return matrix4x4 {x, 0.f, 0.f, 0.f,
 					0.f, y, 0.f, 0.f,
 					0.f, 0.f, z, 0.f,
@@ -185,7 +186,7 @@ DINLINE matrix4x4 createScaleMatrix(float x, float y, float z) {
 // -----------------------------------------------
 // @denpa: Rotates an object around the X axis. (Euler angle)
 // -----------------------------------------------
-DINLINE matrix4x4 createRotationMatrixXAxis(float radian) {
+INTERNAL DINLINE matrix4x4 createRotationMatrixXAxis(f32 radian) {
 	return matrix4x4 {1.f, 0.f, 0.f, 0.f,
 					0.f, cosf(radian), -sinf(radian), 0.f,
 					0.f, sinf(radian), cosf(radian), 0.f,
@@ -195,7 +196,7 @@ DINLINE matrix4x4 createRotationMatrixXAxis(float radian) {
 // -----------------------------------------------
 // @denpa: Rotates an object around the Y axis. (Euler angle)
 // -----------------------------------------------
-DINLINE matrix4x4 createRotationMatrixYAxis(float radian) {
+INTERNAL DINLINE matrix4x4 createRotationMatrixYAxis(f32 radian) {
 	return matrix4x4 {cosf(radian), 0.f, sinf(radian), 0.f,
 					0.f, 1.f, 0.f, 0.f,
 					-sinf(radian), 0.f, cosf(radian), 0.f,
@@ -205,7 +206,7 @@ DINLINE matrix4x4 createRotationMatrixYAxis(float radian) {
 // -----------------------------------------------
 // @denpa: Rotates an object around the Z axis. (Euler angle)
 // -----------------------------------------------
-DINLINE matrix4x4 createRotationMatrixZAxis(float radian) {
+INTERNAL DINLINE matrix4x4 createRotationMatrixZAxis(f32 radian) {
 	return matrix4x4 {cosf(radian), -sinf(radian), 0.f, 0.f,
 					sinf(radian), cosf(radian), 0.f, 0.f,
 					0.f, 0.f, 1.f, 0.f,
@@ -215,7 +216,7 @@ DINLINE matrix4x4 createRotationMatrixZAxis(float radian) {
 // -----------------------------------------------
 // @denpa: Stretches out an object using a shear matrix.
 // -----------------------------------------------
-DINLINE matrix4x4 createShearMatrix(float xy, float xz, float yx, float yz, float zx, float zy) {
+INTERNAL DINLINE matrix4x4 createShearMatrix(f32 xy, f32 xz, f32 yx, f32 yz, f32 zx, f32 zy) {
 	return matrix4x4 {1.f, xy, xz, 0.f,
 					yx, 1, yz, 0.f,
 					zx, zy, 1.f, 0.f,
@@ -226,7 +227,7 @@ DINLINE matrix4x4 createShearMatrix(float xy, float xz, float yx, float yz, floa
 // @denpa: Transposes the matrix provided.
 // A transposed matrix has its rows and columns switched.
 // -----------------------------------------------
-DINLINE matrix4x4 transposeMatrix4x4(matrix4x4 a) {
+INTERNAL DINLINE matrix4x4 transposeMatrix4x4(matrix4x4 a) {
 	return matrix4x4 {a.v[0], a.v[4], a.v[8], a.v[12],
 					a.v[1], a.v[5], a.v[9], a.v[13],
 					a.v[2], a.v[6], a.v[10], a.v[14],
@@ -235,15 +236,15 @@ DINLINE matrix4x4 transposeMatrix4x4(matrix4x4 a) {
 
 // -----------------------------------------------
 // @denpa: Finds the inverse of a 4x4 matrix.
-// j is used as the row in cofactorOf4x4 in order to transpose the matrix. TODO: use only i?
+// j is used as the row in cofactorOf4x4 in order to transpose the matrix.
 // -----------------------------------------------
-DINLINE matrix4x4 inverseMatrix4x4(matrix4x4 a) {
-	float determinant = determinant4x4(a);
+INTERNAL DINLINE matrix4x4 inverseMatrix4x4(matrix4x4 a) {
+	f32 determinant = determinant4x4(a);
 	
 	if (areFloatsEqual(determinant, 0.f)) {return matrix4x4 {};}
 	
 	matrix4x4 result;
-	for (uint32_t i = 0, j = 0; i < 16; i+= 4, j++) {
+	for (u32 i = 0, j = 0; i < 16; i+= 4, j++) {
 		result.v[i] = cofactorOf4x4(a, j, 0) / determinant;
 		result.v[i+1] = cofactorOf4x4(a, j, 1) / determinant;
 		result.v[i+2] = cofactorOf4x4(a, j, 2) / determinant;
@@ -253,9 +254,9 @@ DINLINE matrix4x4 inverseMatrix4x4(matrix4x4 a) {
 }
 
 // -----------------------------------------------
-// @denpa: Multiplies two 4 by 4 matrices. TODO: Maybe use a proper loop? Optimize more?
+// @denpa: Multiplies two 4 by 4 matrices.
 // -----------------------------------------------
-DINLINE matrix4x4 multiplyMatrices4x4(matrix4x4 a, matrix4x4 b) {
+INTERNAL DINLINE matrix4x4 multiplyMatrices4x4(matrix4x4 a, matrix4x4 b) {
 	return matrix4x4 {(a.v[0]*b.v[0]) + (a.v[1]*b.v[4]) + (a.v[2]*b.v[8]) + (a.v[3]*b.v[12]),
 					(a.v[0]*b.v[1]) + (a.v[1]*b.v[5]) + (a.v[2]*b.v[9]) + (a.v[3]*b.v[13]),
 					(a.v[0]*b.v[2]) + (a.v[1]*b.v[6]) + (a.v[2]*b.v[10]) + (a.v[3]*b.v[14]),
@@ -280,7 +281,7 @@ DINLINE matrix4x4 multiplyMatrices4x4(matrix4x4 a, matrix4x4 b) {
 // -----------------------------------------------
 // @denpa: Returns a 4 by 4 identity matrix.
 // -----------------------------------------------
-DINLINE matrix4x4 identityMatrix4x4(void) {
+INTERNAL DINLINE matrix4x4 identityMatrix4x4(void) {
 	return matrix4x4 {1.f, 0.f, 0.f, 0.f,
 					0.f, 1.f, 0.f, 0.f,
 					0.f, 0.f, 1.f, 0.f,
@@ -290,7 +291,7 @@ DINLINE matrix4x4 identityMatrix4x4(void) {
 // -----------------------------------------------
 // @denpa: Multiplies one 4 by 4 matrix with a 4 by 1 tuple.
 // -----------------------------------------------
-DINLINE tuple multiplyMatrix4x4Tuple(matrix4x4 a, tuple b) {
+INTERNAL DINLINE tuple multiplyMatrix4x4Tuple(matrix4x4 a, tuple b) {
 	return tuple {.x = (a.v[0]*b.x) + (a.v[1]*b.y) + (a.v[2]*b.z) + (a.v[3]*b.w),
 				.y = (a.v[4]*b.x) + (a.v[5]*b.y) + (a.v[6]*b.z) + (a.v[7]*b.w),
 				.z = (a.v[8]*b.x) + (a.v[9]*b.y) + (a.v[10]*b.z) + (a.v[11]*b.w),
