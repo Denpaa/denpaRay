668779c4d882ed882637f96d5b1ee8e22c05c9d2
電波 <piacern@gmail.com>
2024-08-11 12:24:39 +0900

    build.bat

---
+++ build.bat
@@ -0,0 +1,1 @@
+@clang++ main.cpp -o denpaRay.exe -O3 -std=c++2b -Weverything -Wno-c99-extensions -Wno-c++98-compat-pedantic -Wno-old-style-cast -Wno-zero-as-null-pointer-constant -Wno-c11-extensions -Wno-gnu-anonymous-struct -ftrivial-auto-var-init=unitialized
